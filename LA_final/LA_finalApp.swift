12fef60ebfe6e43b121b8e5ec75900699b43c7ec
kia-yamaguchi <aika626311@gmail.com>
2026-09-11 23:08:57 +0900

    Initial commit

---
+++ LA_final/LA_finalApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct LA_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LA_finalTests/LA_finalTests.swift
@@ -0,0 +1,11 @@
+import Testing
+
+struct LA_finalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
